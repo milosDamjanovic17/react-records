de3e8e9c9d1ff0124e3b4856fb34a864bbb1bccb
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-22 20:54:21 +0200

    Completed SpinnerLoading util component

--- 02-frontend/react-records/src/Utils/SpinnerLoading.tsx
+++ 02-frontend/react-records/src/Utils/SpinnerLoading.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const SpinnerLoading = () => {
-
-   return(
-   <>
-   </>
-   )
-}
+  
+   return (
+    <div
+      className="container m-5 d-flex justify-content-center"
+      style={{ height: 550 }}
+    >
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
 export default SpinnerLoading;
