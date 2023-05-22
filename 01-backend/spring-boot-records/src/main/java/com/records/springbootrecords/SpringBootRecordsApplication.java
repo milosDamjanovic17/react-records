ed61b78f8467cc4febe15b567b0f18bef72829af
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-22 19:42:28 +0200

    Added Records Spring Boot backend wired backend entity Record to record table

---
+++ 01-backend/spring-boot-records/src/main/java/com/records/springbootrecords/SpringBootRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.records.springbootrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecordsApplication.class, args);
+	}
+
+}
---
+++ 01-backend/spring-boot-records/src/test/java/com/records/springbootrecords/SpringBootRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.records.springbootrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
