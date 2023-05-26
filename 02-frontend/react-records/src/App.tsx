93f4232000f16aa1f40c1d88a1f0a6812c9867d8
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-26 20:01:43 +0200

    Added routing for home and search page --v1.00

--- 02-frontend/react-records/src/App.tsx
+++ 02-frontend/react-records/src/App.tsx
@@ -4,13 +4,23 @@ import Navbar from './layouts/NavbarAndFooter/Navbar';
 import Footer from './layouts/NavbarAndFooter/Footer';
 import HomePage from './layouts/HomePage/HomePage';
 import SearchBooksPage from './layouts/SearchBooks/SearchRecordsPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export function App() {
   return (
     <>
       <Navbar />
-        {/* <HomePage /> */}
+      <Switch>
+      <Route path='/' exact>
+        <Redirect to='/home' />
+      </Route>
+      <Route path='/home'>
+        <HomePage />
+      </Route>
+      <Route path='/search'>
         <SearchBooksPage />
+      </Route>
+      </Switch>
       <Footer />
     </>
   );
