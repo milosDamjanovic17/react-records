85b386ec6143553c91d6ed5d7e294c7aff551234
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-28 01:04:39 +0200

    Completed auth functionality with OKTA

---
+++ 02-frontend/react-records/src/lib/oktaConfig.ts
@@ -0,0 +1,10 @@
+const oktaConfig = {
+   clientId: '0oa9qbfexgdniQnjG5d7',
+   issuer: 'https://dev-76356444.okta.com/oauth2/default',
+   redirectUri: 'http://localhost:3000/login/callback',
+   scopes: ['openid', 'profile', 'email'],
+   pkce: true,
+   disableHttpsCheck: true
+}
+
+export default oktaConfig
