ce7967bce1c43e9cbc548d956d5fcde6b48d3bcc
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-28 11:17:58 +0200

    Completed auth functionality across HomePage btns --v1.00

--- 02-frontend/react-records/src/layouts/HomePage/components/RecordsServices.tsx
+++ 02-frontend/react-records/src/layouts/HomePage/components/RecordsServices.tsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
 
 const RecordsServices = () => {
+
+  const {authState} = useOktaAuth();
+
+  const authCheck = () => {
+    return (
+      authState?.isAuthenticated ? 
+      (
+        <Link type="button" className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white" to="#">
+          Library Services
+        </Link>
+      ) 
+        : 
+      (
+        <Link className="btn main-color btn-lg text-white" to="/login">
+          Sign up
+        </Link>
+      )
+    )
+  };
+
   return (
     <div className="container my-5">
       <div className="row p-4 align-items-center border shadow-lg">
@@ -12,9 +34,7 @@ const RecordsServices = () => {
             If you don't see what you are looking for, send us a message!
           </p>
           <div className="d-grid gap-2 justify-content-md-start mb-4 mb-3">
-            <a className="btn main-color btn-lg text-white" href="#">
-              Sign up
-            </a>
+            {authCheck()}
           </div>
         </div>
         <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
